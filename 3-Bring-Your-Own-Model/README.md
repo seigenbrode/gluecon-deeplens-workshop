b9aa432024f261cd3bef5ace838dded09cf4c7ba
Randy DeFauw <rddefauw@amazon.com>
2019-05-21 16:17:15 -0600

    rework module 3 to use CIFAR10

--- 3-Bring-Your-Own-Model/README.md
+++ 3-Bring-Your-Own-Model/README.md
@@ -2,30 +2,36 @@
 
 ## Module Objective
 In this section of the workshop, we are going to train a custom model using Amazon SageMaker and then deploy that model to
-AWS DeepLens for inference at the edge.  We are going to use MXNet Deep Learning framework with a pretrained [SqueezeNet](https://arxiv.org/abs/1602.07360) image classification model to build/train our own 
-classifier for identifying hot dogs. 
-
-![](images/hotdog.jpg)
+AWS DeepLens for inference at the edge.  We are going to use MXNet Deep Learning framework with a pretrained ResNet model
+and build a classifier for the [CIFAR-10](https://www.cs.toronto.edu/~kriz/cifar.html) data set.
 
 The basic steps involved include: 
 
 1. **Model Training:** Training your model using Amazon SageMaker and export model artifacts to S3
 2. **Import Model to AWS DeepLens:** Import your model artifacts from S3 and deploy it to your AWS DeepLens device
-3. **Evaluate Model Performance:** Evaluate your model using hot dog pictures
+3. **Evaluate Model Performance:** Evaluate your model by looking at the DeepLens output
 
-
- 
 ## Model Training
 
 1. Using your browser, open the Amazon SageMaker console at https://console.aws.amazon.com/sagemaker/.
 2. Sign in to the console using the ID & credentials provided as part of the workshop.
 3. Choose **Notebook Instances** from the left hand menu, then choose **Open Jupyter** next to the notebook you created in the earlier lab
-4. Follow the steps outlined in [**STEP 3. Import and configure an external notebook**](https://aws.amazon.com/getting-started/tutorials/build-deeplens-project-sagemaker/) to *Import and configure an external notebook*
+4. Import the notebook `mxnet_cifar10_with_gluon` from this repository.  
+5. Trust the notebook after the import is complete.
+6. Upload the python scripts `cifar10.py` and `cifar10_utils.py` into the same folder on your notebook instance.
+7. Create a new S3 bucket with a name starting with `deeplens`.  Enter the name of this bucket in the first cell block for the variable `bucket`.
+8. Execute all the cells in the notebook.  The training job will take about 10 minutes to finish.
 
 ## Import Model to AWS DeepLens
 
- 1. Follow the steps outlined in [**STEP 4. Import the model to AWS DeepLens**](https://aws.amazon.com/getting-started/tutorials/build-deeplens-project-sagemaker/) to *Import and configure an external notebook*
+ 1. Follow the steps outlined in [Import Your Amazon SageMaker Trained Model](https://docs.aws.amazon.com/deeplens/latest/dg/deeplens-import-from-sagemaker.html) to import your model to DeepLens.
+ 2. Follow the steps outlined in [Create and Publish an Inference Lambda Function](https://docs.aws.amazon.com/deeplens/latest/dg/deeplens-inference-lambda-create.html) to create the function that will run on your DeepLens.  Use the code in the file `lambda.py` for the function.
+ 3. Follow the steps outlined in [Create and Deploy a Custom AWS DeepLens Project] to publish your new project to the DeepLens.
  
  *Note:* When importing the model choose MXNet as the **Model Framework**
 
 ## Evaluate Model Performance
+
+After publishing the project, look in the `Project output` section of the DeepLens console.  Follow the instructions there to look at the output of the CIFAR10 model in the IoT console.  You'll see JSON-formatted messages that show the probability of each of the 10 classes in the CIFAR10 data set.  
+
+Try pointing your DeepLens at some objects like a car or an airplane and see if the results match up (your lab instructors may have some toy cars and planes you can borrow).  But be aware that we only trained the model for 5 epochs to save time, so the results may not be very impressive yet.
---
+++ 3-Bring-Your-Own-Model/lambda.py
@@ -0,0 +1,147 @@
+#
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+
+# greengrassHelloWorld.py
+# Demonstrates a simple publish to a topic using Greengrass core sdk
+# This lambda function will retrieve underlying platform information and send
+# a hello world message along with the platform information to the topic 'hello/world'
+# The function will sleep for five seconds, then repeat.  Since the function is
+# long-lived it will run forever when deployed to a Greengrass core.  The handler
+# will NOT be invoked in our example since the we are executing an infinite loop.
+
+#*****************************************************
+#                                                    *
+# Copyright 2018 Amazon.com, Inc. or its affiliates. *
+# All Rights Reserved.                               *
+#                                                    *
+#*****************************************************
+""" A sample lambda for CIFAR object detection"""
+from threading import Thread, Event
+import os
+import json
+import numpy as np
+import awscam
+import cv2
+import greengrasssdk
+import mo
+
+class LocalDisplay(Thread):
+    """ Class for facilitating the local display of inference results
+        (as images). The class is designed to run on its own thread. In
+        particular the class dumps the inference results into a FIFO
+        located in the tmp directory (which lambda has access to). The
+        results can be rendered using mplayer by typing:
+        mplayer -demuxer lavf -lavfdopts format=mjpeg:probesize=32 /tmp/results.mjpeg
+    """
+    def __init__(self, resolution):
+        """ resolution - Desired resolution of the project stream """
+        # Initialize the base class, so that the object can run on its own
+        # thread.
+        super(LocalDisplay, self).__init__()
+        # List of valid resolutions
+        RESOLUTION = {'1080p' : (1920, 1080), '720p' : (1280, 720), '480p' : (858, 480)}
+        if resolution not in RESOLUTION:
+            raise Exception("Invalid resolution")
+        self.resolution = RESOLUTION[resolution]
+        # Initialize the default image to be a white canvas. Clients
+        # will update the image when ready.
+        self.frame = cv2.imencode('.jpg', 255*np.ones([640, 480, 3]))[1]
+        self.stop_request = Event()
+
+    def run(self):
+        """ Overridden method that continually dumps images to the desired
+            FIFO file.
+        """
+        # Path to the FIFO file. The lambda only has permissions to the tmp
+        # directory. Pointing to a FIFO file in another directory
+        # will cause the lambda to crash.
+        result_path = '/tmp/results.mjpeg'
+        # Create the FIFO file if it doesn't exist.
+        if not os.path.exists(result_path):
+            os.mkfifo(result_path)
+        # This call will block until a consumer is available
+        with open(result_path, 'w') as fifo_file:
+            while not self.stop_request.isSet():
+                try:
+                    # Write the data to the FIFO file. This call will block
+                    # meaning the code will come to a halt here until a consumer
+                    # is available.
+                    fifo_file.write(self.frame.tobytes())
+                except IOError:
+                    continue
+
+    def set_frame_data(self, frame):
+        """ Method updates the image data. This currently encodes the
+            numpy array to jpg but can be modified to support other encodings.
+            frame - Numpy array containing the image data of the next frame
+                    in the project stream.
+        """
+        ret, jpeg = cv2.imencode('.jpg', cv2.resize(frame, self.resolution))
+        if not ret:
+            raise Exception('Failed to set frame data')
+        self.frame = jpeg
+
+    def join(self):
+        self.stop_request.set()
+
+def infinite_infer_run():
+    """ Entry point of the lambda function"""
+    try:
+        model_type = 'classification'
+        output_map = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 
+            5: 'dog', 6: 'frog', 7: 'horse', 8: 'ship', 9: 'truck'}
+        # Create an IoT client for sending to messages to the cloud.
+        client = greengrasssdk.client('iot-data')
+        iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
+        # Create a local display instance that will dump the image bytes to a FIFO
+        # file that the image can be rendered locally.
+        local_display = LocalDisplay('480p')
+        local_display.start()
+        
+        error, model_path = mo.optimize('model', 32, 32)
+        #model_path = '/opt/awscam/artifacts/mxnet_resnet18-catsvsdogs_FP32_FUSED.xml'
+        # Load the model onto the GPU.
+        client.publish(topic=iot_topic, payload='Loading cifar model')
+        model = awscam.Model(model_path, {'GPU': 1})
+        client.publish(topic=iot_topic, payload='cifar model loaded')
+        
+        num_top_k = 10
+        # The height and width of the training set images
+        input_height = 32
+        input_width = 32
+        # Do inference until the lambda is killed.
+        while True:
+            # Get a frame from the video stream
+            ret, frame = awscam.getLastFrame()
+            if not ret:
+                raise Exception('Failed to get frame from the stream')
+            # Resize frame to the same size as the training set.
+            frame_resize = cv2.resize(frame, (input_height, input_width))
+            # Run the images through the inference engine and parse the results using
+            # the parser API, note it is possible to get the output of doInference
+            # and do the parsing manually, but since it is a classification model,
+            # a simple API is provided.
+            parsed_inference_results = model.parseResult(model_type,
+                                                         model.doInference(frame_resize))
+            # Get top k results with highest probabilities
+            top_k = parsed_inference_results[model_type][0:num_top_k]
+            # Add the label of the top result to the frame used by local display.
+            # See https://docs.opencv.org/3.4.1/d6/d6e/group__imgproc__draw.html
+            # for more information about the cv2.putText method.
+            # Method signature: image, text, origin, font face, font scale, color, and tickness
+            cv2.putText(frame, output_map[top_k[0]['label']], (10, 70),
+                        cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 165, 20), 8)
+            # Set the next frame in the local display stream.
+            local_display.set_frame_data(frame)
+            # Send the top k results to the IoT console via MQTT
+            cloud_output = {}
+            for obj in top_k:
+                cloud_output[output_map[obj['label']]] = obj['prob']
+            client.publish(topic=iot_topic, payload=json.dumps(cloud_output))
+    except Exception as ex:
+        client.publish(topic=iot_topic, payload='Error in cat-dog lambda: {}'.format(ex))
+
+infinite_infer_run()
+
+
